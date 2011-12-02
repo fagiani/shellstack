2c74732e5fe64e0f6f567783a449ceffa633b4ed
Paulo Fagiani <pfagiani@gmail.com>
2011-12-02 14:55:15 -0300

    fixing paths along

--- shellstack.sh
+++ shellstack.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-##Uncomment below to indicate where
-##library is installed, so we can source sub-modules properly
-#SHELLSTACK_INSTALL_DIR=/usr/local/lib/shellstack
-
-if [ -z "$SHELLSTACK_INSTALL_DIR" ] ; then
-	SHELLSTACK_INSTALL_DIR="./lib"
-fi
-
-export SHELLSTACK_INSTALL_DIR
-
-source "$SHELLSTACK_INSTALL_DIR/constants.sh"
-source "$SHELLSTACK_INSTALL_DIR/hostname.sh"
-source "$SHELLSTACK_INSTALL_DIR/user.sh"
-source "$SHELLSTACK_INSTALL_DIR/utils.sh"
-source "$SHELLSTACK_INSTALL_DIR/security.sh"
-source "$SHELLSTACK_INSTALL_DIR/mysql.sh"
-source "$SHELLSTACK_INSTALL_DIR/postgresql.sh"
-source "$SHELLSTACK_INSTALL_DIR/nginx.sh"
-source "$SHELLSTACK_INSTALL_DIR/git.sh"
-source "$SHELLSTACK_INSTALL_DIR/wordpress.sh"
-source "$SHELLSTACK_INSTALL_DIR/ruby.sh"
-source "$SHELLSTACK_INSTALL_DIR/php.sh"
+source "$ROOT_PATH/constants.sh"
+source "$ROOT_PATH/hostname.sh"
+source "$ROOT_PATH/user.sh"
+source "$ROOT_PATH/utils.sh"
+source "$ROOT_PATH/security.sh"
+source "$ROOT_PATH/mysql.sh"
+source "$ROOT_PATH/postgresql.sh"
+source "$ROOT_PATH/nginx.sh"
+source "$ROOT_PATH/git.sh"
+source "$ROOT_PATH/wordpress.sh"
+source "$ROOT_PATH/ruby.sh"
+source "$ROOT_PATH/php.sh"
