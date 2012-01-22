fea5c28c28644901279af9cfb9899b1829600170
Paulo Fagiani <pfagiani@gmail.com>
2012-01-22 18:29:28 -0300

    trying without apache2 mod

--- lib/passenger.sh
+++ lib/passenger.sh
@@ -3,7 +3,7 @@
 function install_passenger_with_nginx {
   add-apt-repository ppa:brightbox/passenger-nginx
   apt-get -y update
-  apt-get -y install libapache2-mod-passenger nginx-full
+  apt-get -y install nginx-full
   cat <<EOF > /etc/nginx/conf.d/passenger.conf
 passenger_root /usr/lib/phusion-passenger;
 passenger_ruby /usr/local/bin/ruby;
